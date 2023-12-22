d63b22db8f4706ba65e201ec712919922697fa22
Avery Larsen <averylarsen@mailbox.org>
2023-12-22 15:36:23 -0800

    Rewrite tests to use axum-test

--- tests/app.rs
+++ tests/app.rs
@@ -1,136 +1,155 @@
-use std::net::SocketAddr;
 use std::time::Duration;
 
-use axum::body::Body;
+use axum_test::TestServer;
 use conduit::{app, jwt, AppState, MockDb};
-use http::{header, Method, Response};
-use http_body_util::BodyExt;
-use hyper::body::Incoming;
-use hyper::client::conn::http1::handshake;
-use hyper::client::conn::http1::SendRequest;
-use hyper::{Request, StatusCode};
-use hyper_util::rt::TokioIo;
+use http::{header, StatusCode};
 use jsonwebtoken::{DecodingKey, EncodingKey};
 use serde_json::{json, Value};
-use tokio::net::{TcpListener, TcpStream};
 
-async fn spawn_server() -> SocketAddr {
-    let listener = TcpListener::bind("0.0.0.0:0").await.unwrap();
-    let address = listener.local_addr().unwrap();
+const TEST_USERNAME: &str = "AzureDiamond";
+const TEST_EMAIL: &str = "example@example.com";
+const TEST_PASSWORD: &str = "hunter2";
 
-    tokio::spawn(async move {
-        axum::serve(listener, app(state())).await.unwrap();
+#[tokio::test]
+async fn test_registration() {
+    let server = new_test_server();
+
+    let user = json!({
+        "username": TEST_USERNAME,
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD
     });
 
-    address
-}
+    let response = server.post("/api/users").json(&user).await;
+    let body: Value = response.json();
 
-struct Client {
-    address: SocketAddr,
-    sender: SendRequest<Body>,
-}
-
-impl Client {
-    async fn from_addr(address: SocketAddr) -> Self {
-        let stream = TcpStream::connect(address).await.unwrap();
-        let io = TokioIo::new(stream);
-
-        let (sender, conn) = handshake(io).await.unwrap();
-
-        tokio::task::spawn(async move {
-            if let Err(err) = conn.await {
-                println!("Connection failed: {:?}", err);
-            }
-        });
-
-        Self { address, sender }
-    }
-
-    async fn get_current_user(&mut self, token: Option<&str>) -> Response<Incoming> {
-        let builder = Request::builder()
-            .uri(format!("http://{}/api/user", self.address))
-            .header(header::HOST, "localhost");
-        let builder = if let Some(token) = token {
-            builder.header(header::AUTHORIZATION, format!("Token {token}"))
-        } else {
-            builder
-        };
-        let request = builder.body(Body::empty()).unwrap();
-
-        self.sender.send_request(request).await.unwrap()
-    }
-
-    async fn register(&mut self) -> Response<Incoming> {
-        let request = Request::builder()
-            .uri(format!("http://{}/api/users", self.address))
-            .header(header::HOST, "localhost")
-            .method(Method::POST)
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(
-                serde_json::to_vec(&json!({
-                    "username": "example",
-                    "email": "example@example.com",
-                    "password": "example"
-                }))
-                .unwrap(),
-            ))
-            .unwrap();
-
-        self.sender.send_request(request).await.unwrap()
-    }
-
-    async fn login(&mut self) -> Response<Incoming> {
-        let request = Request::builder()
-            .uri(format!("http://{}/api/users/login", self.address))
-            .header(header::HOST, "localhost")
-            .method(Method::POST)
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(
-                serde_json::to_vec(&json!({
-                    "username": "example",
-                    "password": "example"
-                }))
-                .unwrap(),
-            ))
-            .unwrap();
-
-        self.sender.send_request(request).await.unwrap()
-    }
-}
-
-fn state() -> AppState {
-    AppState {
-        jwt: jwt::Config {
-            expiration: Duration::from_secs(3600),
-            encoding_key: EncodingKey::from_secret("secret".as_bytes()),
-            decoding_key: DecodingKey::from_secret("secret".as_bytes()),
-        },
-        db: MockDb::default(),
-    }
+    assert_eq!(response.status_code(), StatusCode::CREATED);
+    assert_eq!(
+        response.header(header::CONTENT_TYPE),
+        mime::APPLICATION_JSON.as_ref()
+    );
+    assert_eq!(body["username"], user["username"]);
+    assert_eq!(body["email"], user["email"]);
+    assert!(body["token"].is_string());
 }
 
 #[tokio::test]
-async fn test_authorization() {
-    let address = spawn_server().await;
-    let mut client = Client::from_addr(address).await;
+async fn test_registration_existing_credentials() {
+    let server = new_test_server();
 
-    let response = client.get_current_user(None).await;
+    let user = json!({
+        "username": TEST_USERNAME,
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD
+    });
 
-    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+    let _ = server.post("/api/users").json(&user).await;
 
-    let response = client.register().await;
+    let response = server.post("/api/users").json(&user).await;
 
-    assert_eq!(response.status(), StatusCode::CREATED);
+    assert_eq!(response.status_code(), StatusCode::UNPROCESSABLE_ENTITY);
+}
 
-    let response = client.login().await;
+#[tokio::test]
+async fn test_login() {
+    let server = new_test_server();
 
-    assert_eq!(response.status(), StatusCode::OK);
+    let user = json!({
+        "username": TEST_USERNAME,
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD
+    });
 
-    let body = response.into_body().collect().await.unwrap().to_bytes();
-    let body: Value = serde_json::from_slice(&body).unwrap();
+    let _ = server.post("/api/users").json(&user).await;
+
+    let login_user = json!({
+        "username": TEST_USERNAME,
+        "password": TEST_PASSWORD
+    });
+
+    let response = server.post("/api/users/login").json(&login_user).await;
+    let body: Value = response.json();
+
+    assert_eq!(response.status_code(), StatusCode::OK);
+    assert_eq!(
+        response.header(header::CONTENT_TYPE),
+        mime::APPLICATION_JSON.as_ref()
+    );
+    assert_eq!(body["username"], user["username"]);
+    assert_eq!(body["email"], user["email"]);
+    assert!(body["token"].is_string());
+}
+
+#[tokio::test]
+async fn test_login_invalid_credentials() {
+    let server = new_test_server();
+
+    let user = json!({
+        "username": TEST_USERNAME,
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD
+    });
+
+    let _ = server.post("/api/users").json(&user).await;
+
+    let login_user = json!({
+        "username": TEST_USERNAME,
+        "password": "incorrect"
+    });
+
+    let response = server.post("/api/users/login").json(&login_user).await;
+
+    assert_eq!(response.status_code(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn test_get_current_user() {
+    let server = new_test_server();
+
+    let user = json!({
+        "username": TEST_USERNAME,
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD
+    });
+
+    let response = server.post("/api/users").json(&user).await;
+    let body: Value = response.json();
     let token = body["token"].as_str().unwrap();
 
-    let response = client.get_current_user(Some(token)).await;
+    let response = server
+        .get("/api/user")
+        .add_header(
+            header::AUTHORIZATION,
+            format!("Token {token}").try_into().unwrap(),
+        )
+        .await;
+    let body: Value = response.json();
 
-    assert_eq!(response.status(), StatusCode::OK);
+    assert_eq!(response.status_code(), StatusCode::OK);
+    assert_eq!(
+        response.header(header::CONTENT_TYPE),
+        mime::APPLICATION_JSON.as_ref()
+    );
+    assert_eq!(body["username"], user["username"]);
+    assert_eq!(body["email"], user["email"]);
+    assert!(body["token"].is_string());
+}
+
+#[tokio::test]
+async fn test_get_current_user_no_token() {
+    let server = new_test_server();
+    let response = server.get("/api/user").await;
+    assert_eq!(response.status_code(), StatusCode::UNAUTHORIZED);
+}
+
+fn new_test_server() -> TestServer {
+    let app_state = AppState {
+        jwt: jwt::Config {
+            expiration: Duration::from_secs(3600),
+            encoding_key: EncodingKey::from_secret(b"secret"),
+            decoding_key: DecodingKey::from_secret(b"secret"),
+        },
+        db: MockDb::default(),
+    };
+    TestServer::new(app(app_state)).unwrap()
 }
