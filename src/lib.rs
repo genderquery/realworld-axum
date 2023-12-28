bbd064e02f6139c2048eae8052c1e0a8980a880c
Avery Larsen <averylarsen@mailbox.org>
2023-12-27 20:34:16 -0800

    Move unit tests to lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,32 @@ pub fn app() -> Router {
     // build our application with a single route
     Router::new().route("/", get(|| async { "Hello, World!" }))
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn hello_world() {
+        let app = app();
+
+        // `Router` implements `tower::Service<Request<Body>>` so we can
+        // call it like any tower service, no need to run an HTTP server.
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
+}
